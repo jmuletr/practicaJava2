96d0b5701e90a7a6bf34ec90ca4577e827aea2ce
jmuletr <jmuletr@gmail.com>
2017-01-31 22:49:51 +0100

    Esquelet

---
+++ src/Polynomial.java
@@ -0,0 +1,49 @@
+
+
+public class Polynomial {
+
+    // Constructor per defecte. Genera un polinomi zero
+    public Polynomial() {
+    }
+
+    // Constructor a partir dels coeficients del polinomi en forma d'array
+    public Polynomial(float[] cfs) {
+    }
+
+    // Constructor a partir d'un string
+    public Polynomial(String s) {
+    }
+
+    // Suma el polinomi amb un altre. No modifica el polinomi actual (this). Genera un de nou
+    public Polynomial add(Polynomial p) {
+        return null;
+    }
+
+    // Multiplica el polinomi amb un altre. No modifica el polinomi actual (this). Genera un de nou
+    public Polynomial mult(Polynomial p2) {
+        return null;
+    }
+
+    // Divideix el polinomi amb un altre. No modifica el polinomi actual (this). Genera un de nou
+    // Torna el quocient i també el residu (ambdós polinomis)
+    public Polynomial[] div(Polynomial p2) {
+       return null;
+    }
+
+    // Troba les arrels del polinomi, ordenades de menor a major
+    public float[] roots() {
+        return null;
+    }
+
+    // Torna "true" si els polinomis són iguals. Això és un override d'un mètode de la classe Object
+    @Override
+    public boolean equals(Object o) {
+        return false;
+    }
+
+    // Torna la representació en forma de String del polinomi. Override d'un mètode de la classe Object
+    @Override
+    public String toString() {
+        return "";
+    }
+}
