0e8d829cb8eb96fd98812c21fb0b5c9ed933d7dc
jmuletr <jmuletr@gmail.com>
2017-02-01 17:33:49 +0100

    1r test no ok

--- src/Polynomial.java
+++ src/Polynomial.java
@@ -1,13 +1,54 @@
-
+import java.util.Arrays;
 
 public class Polynomial {
-
+    private float[] coef;
+    private String coefs;
     // Constructor per defecte. Genera un polinomi zero
     public Polynomial() {
+        this.coefs = "0";
     }
 
     // Constructor a partir dels coeficients del polinomi en forma d'array
     public Polynomial(float[] cfs) {
+        int exp = cfs.length - 1;
+        int fact;
+        for (int i = 0; i < cfs.length; i++) {
+            fact = (int)cfs[i];
+            if (cfs[i] == 0 && exp >= 0){
+                exp--;
+                if (i == cfs.length - 1){
+                    this.coefs = "0";
+                }
+            }else{
+                //nombres positius
+                if (cfs[i] >= 0){
+                    //primer valor sense signe
+                    if (i == 0){
+                        if (exp > 0){
+                            if (fact == 1) {
+                                this.coefs = "x";
+                            }else if (cfs[i] > 1) {
+                                this.coefs = Integer.toString(fact);
+                            }
+                            //exponent 0
+                        }else if (fact == 0){
+                            this.coefs = "0";
+                        }
+                        exp--;
+                    }else if (i > 0){
+                        this.coefs += " + ";
+                        if (exp == 0){
+                            if (fact == 0){
+                                this.coefs = "0";
+                            } else this.coefs += fact;
+                        }
+                    }
+                    //nombres negatius
+                } else if (cfs[i] < 0){
+
+                }
+            }
+        }
     }
 
     // Constructor a partir d'un string
@@ -44,6 +85,6 @@ public class Polynomial {
     // Torna la representació en forma de String del polinomi. Override d'un mètode de la classe Object
     @Override
     public String toString() {
-        return "";
+        return this.coefs;
     }
 }
